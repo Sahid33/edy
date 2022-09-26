db68159e97758ac216cbbac7455b24cd0118692b
Sahid33 <104997652+Sahid33@users.noreply.github.com>
2022-09-26 15:51:48 +0530

    Add files via upload

---
+++ DSA_JS/Array_traversing.js
@@ -0,0 +1,11 @@
+let data = [10,23,34,54,1,2,34,54,76,90, 67,90, 88, 89, 23];
+
+// Using Traversing Arrays
+for (let index = 0; index < data.length; index++) {
+   console.log(`Array ${index} is ${data[index]}`)
+    
+}
+
+// accsing
+let x=10;
+console.log(data[x]);
